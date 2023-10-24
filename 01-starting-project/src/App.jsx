f2f7f9a21545edd66805f85fdab6f7cab21fbe0f
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2023-10-24 19:17:54 -0400

    created components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,24 +1,9 @@
-import reactImg from './assets/react-core-concepts.png'
 import { CORE_CONCEPTS } from './assets/data.js';
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import Header from "./components/Header/Header"
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function Header() {
-  const desc = reactDescriptions[genRandomInt(2)]
-  
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {desc} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
+
+
 
 const CoreConcept = ({image,title,description}) => {
   return (
