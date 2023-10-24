f9490b677f485c2b8b2264466c0b63d10a67c7b8
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2023-10-23 22:07:54 -0400

    completed chapter 45

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png'
+import { CORE_CONCEPTS } from './assets/data.js';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -9,7 +11,7 @@ function Header() {
   
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {desc} React concepts you will need for almost any app you are going to build!
@@ -18,11 +20,37 @@ function Header() {
   );
 }
 
+const CoreConcept = ({image,title,description}) => {
+  return (
+    <div>
+      <li>
+        <img src={image} alt="" />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+
+    </div>
+  )
+}
+
+
+
+
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
